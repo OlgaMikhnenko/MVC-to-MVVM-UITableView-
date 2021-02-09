0e4c501dfa8377d33b3734d6c58f88f3b044a25b
Olga Mikhnenko <olga230397@icloud.com>
2021-02-09 12:41:04 +0300

    MVC version

---
+++ MVC to MVVM (UITableView)/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVC to MVVM (UITableView)
+//
+//  Created by Helga on 09.02.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var name : String
+    var secondName : String
+    var age : Int
+}
---
+++ MVC to MVVM (UITableView)/View/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  MVC to MVVM (UITableView)
+//
+//  Created by Helga on 09.02.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullName: UILabel!
+    
+    @IBOutlet weak var age: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
